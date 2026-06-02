6cbadce460aa52daa92171a4afeb635d2be84ee4
BaburinA <bagik61#mail.ru>
2026-06-02 10:19:58 +0400

    Check police

--- PoliceIB/ib.sh
+++ PoliceIB/ib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ansible-playbook -i ./inv/ib ib_fstek_2022_red8.yml --check --limit $1
+#ansible-playbook -i ./inv/ib ib_fstek_2022_red8.yml --check --limit $1
+# ansible-playbook -i ./inv/ib back_up_crit.yml --limit $1
+ansible-playbook -i ./inv/ib rollback_police.yml --check --limit $1
